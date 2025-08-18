9ac97a80008e5dc3c59154ba62a3fa4d374b2fd8
David Tielke <mail@david-tielke.de>
2025-08-18 14:34:50 +0200

    Add project files.

---
+++ WebStarter/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStarter.Models;
+
+namespace WebStarter.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new List<Person>
+            {
+                new Person(1, "David", "Tielke", 41),
+                new Person(2, "Lena", "Tielke", 39),
+                new Person(3, "Maximilian", "Tielke", 12),
+                new Person(4, "Lisa", "Tielke", 2),
+            };
+
+
+            return View(model);
+        }
+    }
+}
---
+++ WebStarter/Models/Person.cs
@@ -0,0 +1,23 @@
+﻿namespace WebStarter.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public int Age { get; set; }
+
+        public Person()
+        {
+            
+        }
+
+        public Person(int id, string firstname, string lastname, int age)
+        {
+            Id = id;
+            Firstname = firstname;
+            Lastname = lastname;
+            Age = age;
+        }
+    }
+}
